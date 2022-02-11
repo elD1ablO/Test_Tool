76b5ff2653e887bd68da67fd7071ea0f8d117887
Maksym <eldiablo@ukr.net>
2022-02-11 14:00:50 +0200

    UI, buttons and sliders made

--- Assets/Scripts/ToolManager.cs
+++ Assets/Scripts/ToolManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ToolManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject toolScreen;
+    [SerializeField] GameObject toolsButton;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] Slider quantitySlider;
+    [SerializeField] TextMeshProUGUI quantityText;
+
+    [SerializeField] Slider rotationSlider;
+    [SerializeField] TextMeshProUGUI rotationText;
+
+    bool zoneTL, zoneBL, zoneTR, zoneBR;
+
+    private void Awake()
     {
-        
+        toolsButton.SetActive(true);
+        toolScreen.SetActive(false);
+    }
+    public void Update()
+    {
+        UpdateSliders();
+    }
+    
+    //set zone to spawn characters
+    public void SetTopLeft()
+    {
+        zoneTL = true;
+    }
+    public void SetBottomLeft()
+    {
+        zoneBL = true;
+    }
+    public void SetTopRight()
+    {
+        zoneTR = true;
+    }
+    public void SetBottomRight()
+    {
+        zoneBR = true;
+    }
+    // visualizes slider position to text
+    void UpdateSliders()
+    {
+        quantityText.text = quantitySlider.value.ToString();
+        rotationText.text = rotationSlider.value.ToString();
+    }
+    public void ShowTools()
+    {
+        toolScreen.SetActive(true);
+        toolsButton.SetActive(false);
+    }
+    public void Apply()
+    {
+        toolScreen.SetActive(false);
+        toolsButton.SetActive(true);
+    }
+    public void ExitTool()
+    {
+        toolScreen.SetActive(false);
+        toolsButton.SetActive(true);
     }
 }
